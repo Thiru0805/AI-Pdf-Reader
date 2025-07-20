1adbdc55fb238c98f9cbcbdbb3dbfaf59e6d9dbd
Thirumalesh <thirumaleshakash2@gmail.com>
2025-07-20 13:22:41 +0530

    variable chages

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const uploadPDF = async (file: File) => {
   const formData = new FormData();
